1a3fe62edf38e6797a351c790bbe40a80ff5e8e5
Tristan Deschamp <tristan2020.d@gmail.com>
2024-07-31 17:52:57 +0200

    Implement basic world setup with graphics and a basic camera as well as player movement framerate independant.

--- code/main.py
+++ code/main.py
@@ -1,13 +1,48 @@
 from settings import *
+from pytmx.util_pygame import load_pygame
+from os.path import join
+
+from sprites import Sprite
+from entities import Player
+from groups import AllSprites
 
 class Game:
 	def __init__(self):
 		pygame.init()
 		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 		pygame.display.set_caption('PythonMonsters')
+		self.clock = pygame.time.Clock()
+
+		# Groups
+		self.all_sprites = AllSprites()
+
+		self.import_assets()
+		self.setup(self.tmx_maps['hospital'], 'world')
+
+	def import_assets(self):
+		self.tmx_maps = {
+			'world': load_pygame(join('..', 'data', 'maps', 'world.tmx')),
+			'hospital': load_pygame(join('..', 'data', 'maps', 'hospital.tmx')),
+		}
+
+	def setup(self, tmx_map, player_start_pos):
+		# Terrain
+		for layer in ['Terrain', 'Terrain Top']:
+			for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
+				Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+		
+		# Objects
+		for obj in tmx_map.get_layer_by_name('Objects'):
+			Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+
+		# Entities
+		for obj in tmx_map.get_layer_by_name('Entities'):
+			if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
+				self.player = Player((obj.x, obj.y), self.all_sprites)
 
 	def run(self):
 		while True:
+			dt = self.clock.tick() / 1000
 			# Event Loop
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
@@ -15,6 +50,9 @@ class Game:
 					exit()
 
 			# Game Logic
+			self.all_sprites.update(dt)
+			self.display_surface.fill('black')
+			self.all_sprites.draw(self.player.rect.center)
 			pygame.display.update()
 
 if __name__ == '__main__':
